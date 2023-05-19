7e6ed3d64d21c43aa2418aeac184537529f2982d
Adik08 <adik08.eamil@gmail.com>
2023-05-19 23:49:27 +0200

    nie działa przez plik

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //PassengerId	Survived	Pclass	Name	Sex	Age	SibSp	Parch	Ticket	Fare	Cabin	Embarked
-        List<String> linie = Files.readAllLines(Path.of("C:/Users/sirk0/Desktop/tytanik.txt"));
-        for (String linia: linie)
-            System.out.println(linia);
+        List<Pasarzer> pasarzerowie=new ArrayList<>();
+        //PassengerId   Survived    Pclass  Name    Sex	    Age	    SibSp	Parch	Ticket	Fare	Cabin	Embarked
+        List<String> linie = Files.readAllLines(Path.of("C:/Users/sirk0/Documents/odp.txt"));
+        for(int i = 0;i<linie.size();i++) {
+
+                List<String[]> linia = linie.stream().map(x->x.split("\t")).collect(Collectors.toList());
+                Pasarzer pasarzer = new Pasarzer(linia.get(i)[0],linia.get(i)[1],linia.get(i)[2],linia.get(i)[3],linia.get(i)[4],linia.get(i)[5],linia.get(i)[6],linia.get(i)[7],linia.get(i)[8],linia.get(i)[9],linia.get(i)[10],linia.get(i)[11]);
+                pasarzerowie.add(pasarzer);
+        }
+        AtomicInteger sredniWiek= new AtomicInteger(1);
+        pasarzerowie.stream().forEach(x-> sredniWiek.updateAndGet(v -> v * pasarzerowie.get(x.id).age));
+        System.out.println("Ilość pasażerów: "+pasarzerowie.size());
+        System.out.println("Średni wiek pasażerów: "+(sredniWiek.get() /pasarzerowie.size()));
     }
 }
--- src/Pasarzer.java
+++ src/Pasarzer.java
@@ -1,13 +1,14 @@
 public class Pasarzer {
     //PassengerId	Survived	Pclass	Name	Sex	Age	SibSp	Parch	Ticket	Fare	Cabin	Embarked
-    String id, survived, pClass, name, sex, age, sibSp, parch, ticket, fare, cabin, embarked;
+    String survived, pClass, name, sex, sibSp, parch, ticket, fare, cabin, embarked;
+    int age,id;
     public Pasarzer(String id2,String survived2,String pClass2,String name2,String sex2,String age2,String sibSp2,String parch2,String ticket2,String fare2,String cabin2,String embarked2) {
-        id = id2;
+        id = Integer.parseInt(id2);
         survived=survived2;
         pClass=pClass2;
         name=name2;
         sex=sex2;
-        age=age2;
+        age=Integer.getInteger(age2);
         sibSp=sibSp2;
         parch=parch2;
         ticket=ticket2;
