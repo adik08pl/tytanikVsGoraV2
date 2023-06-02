7554919e8fb55440fce9b5048c791e6056292b13
Adik08 <adik08.eamil@gmail.com>
2023-06-03 00:22:24 +0200

    chyba działa jednak

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,12 +15,71 @@ public class Main {
         for(int i = 0;i<linie.size();i++) {
 
                 List<String[]> linia = linie.stream().map(x->x.split("\t")).collect(Collectors.toList());
-                Pasarzer pasarzer = new Pasarzer(linia.get(i)[0],linia.get(i)[1],linia.get(i)[2],linia.get(i)[3],linia.get(i)[4],linia.get(i)[5],linia.get(i)[6],linia.get(i)[7],linia.get(i)[8],linia.get(i)[9],linia.get(i)[10],linia.get(i)[11]);
+                Pasarzer pasarzer = new Pasarzer(linia.get(i)[0],linia.get(i)[1],linia.get(i)[2],linia.get(i)[3],linia.get(i)[4],linia.get(i)[5],linia.get(i)[6],linia.get(i)[7],linia.get(i)[8],linia.get(i)[9],linia.get(i)[10],linia.get(i)[10]);
                 pasarzerowie.add(pasarzer);
         }
-        AtomicInteger sredniWiek= new AtomicInteger(1);
-        pasarzerowie.stream().forEach(x-> sredniWiek.updateAndGet(v -> v * pasarzerowie.get(x.id).age));
+        int sredniWiek = 0;
+        int iloscPasarzerowZWiekiem = 0;
         System.out.println("Ilość pasażerów: "+pasarzerowie.size());
-        System.out.println("Średni wiek pasażerów: "+(sredniWiek.get() /pasarzerowie.size()));
+        for(int i = 0;i<pasarzerowie.size();i++) {
+            try {
+                sredniWiek += Integer.parseInt(pasarzerowie.get(i).age);
+                iloscPasarzerowZWiekiem++;
+            }catch (NumberFormatException n){
+
+            }
+        }
+        System.out.println("Średni wiek: "+sredniWiek / iloscPasarzerowZWiekiem);
+        int kobiety=0,mezczyzni=0;
+        for(int j = 0;j<pasarzerowie.size();j++){
+            if(pasarzerowie.get(j).sex.equals("male")){
+                mezczyzni++;
+            }
+            else if(pasarzerowie.get(j).sex.equals("female")){
+                kobiety++;
+            }
+        }
+        System.out.println("Ilość mężczyzn: "+mezczyzni);
+        System.out.println("Ilość kobiet: "+kobiety);
+        int przezyli=0;
+        for(int k = 0;k<pasarzerowie.size();k++){
+            if(pasarzerowie.get(k).survived.equals("1"))
+                przezyli++;
+        }
+        System.out.println("Ile procent przeżyło: "+100*przezyli / pasarzerowie.size());
+        int pierwszaKlasa=0,drugaKlasa=0,trzeciaKlasa=0;
+        for(int l = 0;l<pasarzerowie.size();l++){
+            if(pasarzerowie.get(l).pClass.equals("1"))
+                pierwszaKlasa++;
+            else if(pasarzerowie.get(l).pClass.equals("2"))
+                drugaKlasa++;
+            else if(pasarzerowie.get(l).pClass.equals("3"))
+                trzeciaKlasa++;
+        }
+        System.out.println("Ilość pasażerów w 1 klasie: "+pierwszaKlasa);
+        System.out.println("Ilość pasażerów w 2 klasie: "+drugaKlasa);
+        System.out.println("Ilość pasażerów w 3 klasie: "+trzeciaKlasa);
+        int mezczyzniKtorzyPrzezyli=0, kobietyKtorePrzezyly=0;
+        for(int m = 0;m<pasarzerowie.size();m++){
+            if(pasarzerowie.get(m).sex.equals("male")&&pasarzerowie.get(m).survived.equals("1")){
+                mezczyzniKtorzyPrzezyli++;
+            }
+            else if(pasarzerowie.get(m).sex.equals("female")&&pasarzerowie.get(m).survived.equals("1")){
+                kobietyKtorePrzezyly++;
+            }
+        }
+        System.out.println("Kobiety które przeżyły: "+kobietyKtorePrzezyly);
+        System.out.println("Mężczyźni którzy przeżyli: "+mezczyzniKtorzyPrzezyli);
+        int nieWypelnionaPlec=0,nieWypelnionaKlasa=0;
+        for(int n = 0;n<pasarzerowie.size();n++){
+            if(!(pasarzerowie.get(n).sex.equals("male")||pasarzerowie.get(n).sex.equals("female"))){
+                nieWypelnionaPlec++;
+            }
+            if(!(pasarzerowie.get(n).pClass.equals("1")||pasarzerowie.get(n).pClass.equals("2")||pasarzerowie.get(n).pClass.equals("3"))){
+                nieWypelnionaKlasa++;
+            }
+        }
+        System.out.println("Ilość pasażerów bez wypełnionej płci: "+nieWypelnionaPlec);
+        System.out.println("Ilość pasażerów bez wypełnionej klasy: "+nieWypelnionaKlasa);
     }
 }
--- src/Pasarzer.java
+++ src/Pasarzer.java
@@ -1,14 +1,13 @@
 public class Pasarzer {
     //PassengerId	Survived	Pclass	Name	Sex	Age	SibSp	Parch	Ticket	Fare	Cabin	Embarked
-    String survived, pClass, name, sex, sibSp, parch, ticket, fare, cabin, embarked;
-    int age,id;
+    String survived, pClass, name, sex, sibSp, parch, ticket, fare, cabin, embarked,age,id;
     public Pasarzer(String id2,String survived2,String pClass2,String name2,String sex2,String age2,String sibSp2,String parch2,String ticket2,String fare2,String cabin2,String embarked2) {
-        id = Integer.parseInt(id2);
+        id =id2;
         survived=survived2;
         pClass=pClass2;
         name=name2;
         sex=sex2;
-        age=Integer.getInteger(age2);
+        age=age2;
         sibSp=sibSp2;
         parch=parch2;
         ticket=ticket2;
